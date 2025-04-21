59b10025e14e00b002c37b93c2a2e142552d0de8
Kaustubh <kaustubhthakur76@gmail.com>
2025-04-22 00:23:16 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,12 @@ const authrouter = require('./routes/auth')
 const userrouter = require('./routes/users')
 const postrouter = require('./routes/posts')
 const snaprouter = require('./routes/snap')
+const cloudinary = require('cloudinary')
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 app.use(express.json())
 app.use(cors())
 app.use(cookieparser());
