eed6d8b2c8bbb3b2be0165806ffcb6204010b489
Kaustubh <kaustubhthakur76@gmail.com>
2025-04-23 22:47:56 +0530

    first commit

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,6 +1,6 @@
 const Post = require('../models/Post')
 const Snap = require('../models/Snap')
-const createpost = async(req,res) =>{
+const createpost = async (req, res) => {
     try {
         const newpost = new Post(req.body);
         const savepost = await newpost.save();
@@ -9,7 +9,7 @@ const createpost = async(req,res) =>{
         console.error(error);
     }
 }
-const getpost = async(req,res)=>{
+const getpost = async (req, res) => {
     try {
         const post = await Post.findById(req.params.id);
         res.status(201).json(post);
@@ -17,7 +17,7 @@ const getpost = async(req,res)=>{
         console.error(error);
     }
 }
-const getposts = async(req,res) =>{
+const getposts = async (req, res) => {
     try {
         const posts = await Post.find();
         res.status(201).json(posts);
@@ -25,10 +25,10 @@ const getposts = async(req,res) =>{
         console.error(error);
     }
 }
-const deletepost = async(req,res)=>{
+const deletepost = async (req, res) => {
     try {
         await Post.findByIdAndDelete(req.params.id);
-        res.status(201).json({message:"post has been deleted..."})
+        res.status(201).json({ message: "post has been deleted..." })
     } catch (error) {
         console.error(error);
     }
@@ -37,48 +37,56 @@ const likeSnap = async (req, res) => {
     try {
         const snapId = req.params.sid;
         const userId = req.user._id;
-        const postId = req.body.id; 
-        
-        
+        const postId = req.body.id;
+
         const snap = await Snap.findById(snapId);
-        
+
         if (!snap) {
             return res.status(404).json({ message: "Snap not found" });
         }
-    
+
         if (postId) {
             const post = await Post.findById(postId);
             if (!post) {
                 return res.status(404).json({ message: "Post not found" });
             }
-            
-            // Make sure this snap is associated with the post
+
+
             if (!post.snaps.includes(snapId)) {
                 return res.status(400).json({ message: "This snap does not belong to the specified post" });
             }
-        }
-        
 
+
+            const allPostSnaps = await Snap.find({ _id: { $in: post.snaps } });
+            const hasVotedOtherSnap = allPostSnaps.some(
+                otherSnap => otherSnap._id.toString() !== snapId && otherSnap.votes.includes(userId)
+            );
+
+            if (hasVotedOtherSnap) {
+                return res.status(400).json({
+                    message: "You have already voted on another snap in this post",
+                    success: false
+                });
+            }
+        }
         if (!snap.votes.includes(userId)) {
-            // Like the snap - add user ID to votes array
+
             await Snap.updateOne(
-                { _id: snapId }, 
+                { _id: snapId },
                 { $push: { votes: userId } }
             );
-            
-            res.status(200).json({ 
+            res.status(200).json({
                 message: "Snap has been liked",
                 success: true,
-                snap: await Snap.findById(snapId) // Return updated snap
+                snap: await Snap.findById(snapId)
             });
         } else {
-            // Unlike the snap - remove user ID from votes array
             await Snap.updateOne(
-                { _id: snapId }, 
+                { _id: snapId },
                 { $pull: { votes: userId } }
             );
-            
-            res.status(200).json({ 
+
+            res.status(200).json({
                 message: "Snap has been unliked",
                 success: true,
                 snap: await Snap.findById(snapId) // Return updated snap
@@ -86,11 +94,11 @@ const likeSnap = async (req, res) => {
         }
     } catch (error) {
         console.error(error);
-        res.status(500).json({ 
-            message: "Server error", 
+        res.status(500).json({
+            message: "Server error",
             success: false,
             error: error.message
         });
     }
 };
-module.exports = {createpost,deletepost,getpost,getposts,likeSnap}
+module.exports = { createpost, deletepost, getpost, getposts, likeSnap }
