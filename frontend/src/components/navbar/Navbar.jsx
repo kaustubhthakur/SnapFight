d8ddadba961b416645f504628b348d8f3deb2a66
Kaustubh <kaustubhthakur76@gmail.com>
2025-05-03 00:20:31 +0530

    first commit

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -12,12 +12,11 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        {/* Logo */}
         <Link to="/" className="navbar-logo">
-          Logo
+          SNAPFF
         </Link>
 
-        {/* Hamburger Menu Icon */}
+       
         <button
           className={`hamburger ${isOpen ? 'active' : ''}`}
           onClick={toggleMenu}
@@ -28,21 +27,8 @@ const Navbar = () => {
           </span>
         </button>
 
-        {/* Navigation Menu */}
-        <div className={`nav-menu ${isOpen ? 'active' : ''}`}>
-          <div className="nav-links">
-            <Link to="/" className="nav-link">
-              Home
-            </Link>
-            <Link to="/about" className="nav-link">
-              About
-            </Link>
-            <Link to="/services" className="nav-link">
-              Services
-            </Link>
-          </div>
-
-          {/* Auth Links */}
+     
+        <div className={`nav-menu ${isOpen ? 'active' : ''}`}>  
           <div className="auth-buttons">
             <Link to="/login" className="btn login">
               Login
