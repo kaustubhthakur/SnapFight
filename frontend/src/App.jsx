4cccf625172e792e6caf78df786b75f07fdf4e9e
Kaustubh <kaustubhthakur76@gmail.com>
2025-06-01 22:21:54 +0530

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/homepage/HomePage";
 import RegisterPage from "./pages/registerpage/RegisterPage";
+import LoginPage from "./pages/loginpage/LoginPage";
+import Profile from "./components/profile/Profile";
+import CreatePost from "./components/createpost/CreatePost";
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<HomePage/>} />
-      <Route path='/register' element={<RegisterPage/>} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/createpost" element={<CreatePost />} />
     </Routes>
   );
 };
