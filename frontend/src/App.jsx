84ebd12302e2d72207df6d6b931461eb34000fe2
Kaustubh <kaustubhthakur76@gmail.com>
2025-06-25 00:01:47 +0530

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,9 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/homepage/HomePage";
-import RegisterPage from "./pages/registerpage/RegisterPage";
-import LoginPage from "./pages/loginpage/LoginPage";
-import Profile from "./components/profile/Profile";
-import CreatePost from "./components/createpost/CreatePost";
+import React from 'react'
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/profile" element={<Profile/>} />
-        <Route path="/createpost" element={<CreatePost />} />
-      </Routes>
-    </BrowserRouter>
-  );
-};
+    <div>App</div>
+  )
+}
 
-export default App;
+export default App
