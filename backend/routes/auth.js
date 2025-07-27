fe6cd317d46352034703196a03b17c27aeca85c2
Kaustubh <kaustubhthakur76@gmail.com>
2025-07-27 22:52:34 +0530

    initial comment

--- backend/routes/auth.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router();
-const {register,login,logout} = require('../controllers/auth')
-router.post('/register',register)
-router.post('/logout',logout)
-router.post('/login',login)
-module.exports = router;
--- backend/routes/snap.js
+++
@@ -1,8 +0,0 @@
-const express =require('express')
-const router = express.Router();
-const upload = require('../utils/upload')
-const {createSnap,getsnap} = require('../controllers/snaps')
-const protectRoute = require('../utils/protectRoute')
-router.get('/:id',protectRoute,getsnap)
-router.post('/:id', protectRoute,createSnap)
-module.exports = router;
